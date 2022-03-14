03da0770968b4b580f9bae73e9149e4017541c4b
Le Vu <levu08072002@gmail.com>
2022-03-14 14:18:22 +0700

    upDATE

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import logo from './logo.svg'
 import './App.css'
 import ShowInfo from './Components/ShowInfo'
 import Item from "./Components/Item"
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Pagination from "@mui/material/Pagination";
 function App() {  
   const [products,setProducts] = useState([
     {id:1,name: "Vu"},
@@ -20,6 +23,8 @@ function App() {
     <button onClick={() => setCount(count - 1)}>Decrease</button>
   {products.map((item,index) => <div>{item.name} <button onClick={()=> removeItem(item.id)}>Remove</button></div> )}
     <ShowInfo name="Vu"/>
+    <Button variant="contained">Hello World</Button>;
+    <Pagination count={10} color="primary" />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-const myName = "Vu"
-const myAge = "13"
-const myStatus = true
-const person = {
-  name : "Le Vu",
-  age : 14,
-  address : "Tuyen Quang"
-}
 
 ReactDOM.render(<App/>,
   document.getElementById('root')
