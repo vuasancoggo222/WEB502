3768fc7d2b01674ebfd034a10eb603b872175f5b
Le Vu <levu08072002@gmail.com>
2022-03-16 14:11:16 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-interface  Product {
-  id : number,
-  name : string,
-}
-function App() {
-  const [count, setCount] = useState<number>(0)
- const [Products,setProducts] = useState<Product[]>([{id: 1, name:"Ha"},{id: 1, name:"Vu"},{id: 1, name:"Nam"},{id: 1, name:"Anh"}])
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
+import ShowInfo from "./components/ShowInfo";
+import type { ProductType } from "./types/product";
+import {getAll,remove} from "./api/product"
+
+const App = () => {
+  const [products,setProducts] = useState<ProductType[]>([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      const { data } = await axios.get("http://localhost:5000/api/products");
+      console.log(data);
+      setProducts(data);
+    };
+    getProducts();
+  },[]);
+  const removeItem = async (id: number) => {
+    const { data } = await remove(id);
+    data && setProducts(products.filter(item => item._id !== data._id));
+  }
   return (
     <div className="App">
-      <div>{Products.map(product=> <div>{product.name}</div>)}</div>
+      <table>
+        <thead>
+            <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Chức năng</th>
+            </tr>
+        </thead>
+        <tbody>
+          {products.map((item, index) => {
+            return <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td><button onClick={() => removeItem(item._id)}>Remove</button></td>
+            </tr>
+          })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
